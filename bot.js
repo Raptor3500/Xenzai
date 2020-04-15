777ce6fc74b3f9bc7a2099652f6e047116fd089b
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2020-04-15 04:16:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.login("Njk5ODE0MTA1MzI2MDI2NzYy.XpbB8w.xteuVTkTMcPIdx5S4OuJnJnrrqA");
+bot.login(Njk5ODE0MTA1MzI2MDI2NzYy.XpbB8w.xteuVTkTMcPIdx5S4OuJnJnrrqA);
